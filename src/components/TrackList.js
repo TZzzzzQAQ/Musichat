eabde9d6e209aad29a293857c625888877ea31b7
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-04-22 17:58:35 +1200

    V1.5.1 fix some bugs

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -4,17 +4,17 @@ import {formatTime} from "@/utils";
 const TrackList = ({data: {name, artists, duration_ms}}) => {
     console.log()
     return (
-        <tr className="hover:bg-gray-100 underline-animation cursor-pointer font-poppins">
-            <td className="px-5 py-2 border-b border-gray-200 bg-white text-lg">
-                <span className="cursor-pointer text-gray-900 whitespace-no-wrap">
+        <tr className="underline-animation cursor-pointer font-poppins">
+            <td className="px-5 py-3 border-b border-gray-200 text-base">
+                <span className="cursor-pointer whitespace-no-wrap">
                 {name}
                 </span>
             </td>
-            <td className="px-5 py-2 border-b border-gray-200 bg-white text-lg">
+            <td className="px-5 py-3 border-b border-gray-200 text-base">
                 <span className="inline-block mr-2 last:mr-0">{artists[0].name}</span>
             </td>
-            <td className="px-5 py-2 border-b border-gray-200 bg-white text-lg">
-                <span className="text-gray-900 whitespace-no-wrap">
+            <td className="px-5 py-3 border-b border-gray-200 text-base">
+                <span className="whitespace-no-wrap">
                   {formatTime(duration_ms / 1000)}
                 </span>
             </td>
