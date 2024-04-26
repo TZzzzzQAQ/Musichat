e7a960ffb424ee816b946c0271a81730ea334a23
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-04-26 17:45:09 +1200

    V1.6.6 hot fix

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-CLIENT_ID='e247748bbbb8477c87470ff5bc581f52';
-CLIENT_SECRET='d9b98db5d845496f9f53d131bc41265e';
+export const client_id = 'e247748bbbb8477c87470ff5bc581f52';
+export const client_secret = 'd9b98db5d845496f9f53d131bc41265e';
