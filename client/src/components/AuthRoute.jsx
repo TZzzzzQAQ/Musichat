4daf1edeca15e500cca418e9f8980fbfb7a45651
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-05-01 13:38:10 +1200

    v3.4.0FrontEnd Add Auth Route for specific  components

--- client/src/components/AuthRoute.jsx
+++ client/src/components/AuthRoute.jsx
@@ -1,13 +1,13 @@
-import {getEveryoneToken} from '@/utils/index.jsx'
+import {getUserToken} from '@/utils/index.jsx'
 import {Navigate} from 'react-router-dom'
 
 const AuthRoute = ({children}) => {
 
-    const isToken = getEveryoneToken()
+    const isToken = getUserToken()
     if (isToken) {
         return <>{children}</>
     } else {
-        return <Navigate to="/login" replace/>
+        return <Navigate to="/account" replace/>
     }
 }
 
