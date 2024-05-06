307e9bf2d7c93dc40b0bdb693b700275c555772b
GFORGALEN <100574277+GFORGALEN@users.noreply.github.com>
2024-05-07 01:30:39 +1200

    Profile's done

--- client/src/pages/Menu/User/Account.jsx
+++ client/src/pages/Menu/User/Account.jsx
@@ -1,65 +1,109 @@
-import {useLocation} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {setUserProfile} from "@/store/features/userSlice.jsx";
-import {useSpotifyAuth} from "@/hooks/useSpotifyAuth.jsx";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserProfile } from "@/store/features/userSlice.jsx";
+import { useSpotifyAuth } from "@/hooks/useSpotifyAuth.jsx";
+import { sendUserDataAPI } from "@/apis/chatGroupAPI.jsx";
+import "./Account.css";
 import Setting from "@/pages/Menu/User/User.jsx";
-import {sendUserDataAPI} from "@/apis/chatGroupAPI.jsx";
+const backgroundImageUrl = "https://i.ibb.co/cTxbCWy/spotify-k8mh.png";
 
+const BackgroundImage = () => (
+  <div
+    className="background-image"
+    style={{ backgroundImage: `url('${backgroundImageUrl}')` }}
+  />
+);
 
-function App() {
-    const location = useLocation();
-    const [profile, setProfile] = useState({});
-    const userState = useSelector(state => state.user);
-    const dispatch = useDispatch();
-    const {redirectToAuthCodeFlow,fetchProfile} = useSpotifyAuth();
-    useEffect(() => {
-        setProfile(userState);
-    }, []);
-
-    useEffect(() => {
-        if (Object.keys(userState).length > 0) {
-            setProfile(userState);
-            sendUserDataAPI(userState)
-        }
-    }, [userState]);
-
-    useEffect(() => {
-        const query = new URLSearchParams(location.search);
-        const code = query.get("code");
-        if (code && Object.keys(profile).length === 0) {
-            handlerClickProfile();
-        }
-    }, [location, profile]);
-
-    const handlerClick = () => {
-        redirectToAuthCodeFlow();
+const LoginHeader = ({ onLogin }) => {
+    const [loginText, setLoginText] = useState("Login to Your Account");
+  
+    const handleClick = () => {
+      setLoginText("Signing in...");
+      setTimeout(() => {
+        onLogin();
+      }, 1000);
     };
-
-    const handlerClickProfile = async () => {
-        const query = new URLSearchParams(location.search);
-        const code = query.get("code");
-        try {
-            const profile = await fetchProfile(code)
-            if (profile?.error?.status === 401) {
-                return
-            }
-            setProfile(profile);
-            dispatch(setUserProfile(profile));
-        } catch (error) {
-            console.error('Failed to fetch profile:', error);
-        }
-    }
+  
     return (
-        <div>
-            <header className="App-header">
-                <button onClick={handlerClick}>点我登录</button>
-                {profile && <div>{JSON.stringify(profile)}</div>}
-            </header>
-            <Setting/>
+      <h2 className="login-header" onClick={handleClick}>
+        {loginText}
+      </h2>
+    );
+  };
+function Account() {
+  const location = useLocation();
+  const userState = useSelector((state) => state.user.profile);
+  const dispatch = useDispatch();
+  const { redirectToAuthCodeFlow, fetchProfile } = useSpotifyAuth();
+
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const code = query.get("code");
+    if (code && Object.keys(userState).length === 0) {
+      handlerFetchProfile(code);
+    }
+  }, [location, userState]);
+
+  const handlerLogin = () => {
+    redirectToAuthCodeFlow();
+  };
+
+  const handlerFetchProfile = async (code) => {
+    try {
+      const profile = await fetchProfile(code);
+      if (profile?.error?.status === 401) {
+        return;
+      }
+      dispatch(setUserProfile(profile));
+      sendUserDataAPI(profile);
+    } catch (error) {
+      console.error("Failed to fetch profile:", error);
+    }
+  };
+
+  return (
+    <div className="account-page">
+      {Object.keys(userState).length === 0 ? (
+        <div className="login-container">
+          <BackgroundImage />
+          <LoginHeader onLogin={handlerLogin} />
         </div>
-    )
-        ;
+      ) : (
+        <div className="profile-container">
+          <h2>Welcome, {userState.display_name}!</h2>
+           <Setting />
+          <div className="profile-info">
+            <img
+              src={userState.images[0]?.url || "default-avatar.png"}
+              alt="Profile"
+              className="profile-avatar"
+            />
+            <div className="profile-details">
+              <p>
+                <strong>Name:</strong> {userState.display_name}
+              </p>
+              <p>
+                <strong>Email:</strong> {userState.email}
+              </p>
+              <p>
+                <strong>Country:</strong> {userState.country}
+              </p>
+              <p>
+                <strong>Spotify ID:</strong> {userState.id}
+              </p>
+              <p>
+                <strong>Subscription:</strong> {userState.product}
+              </p>
+              <p>
+                <strong>Followers:</strong> {userState.followers.total}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App;
+export default Account;
