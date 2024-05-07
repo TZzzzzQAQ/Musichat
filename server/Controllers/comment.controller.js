efcccd200800ed54f352959070d571c358b31aa8
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-05-07 18:13:25 +1200

    V4.6.3 new api

--- server/Controllers/comment.controller.js
+++ server/Controllers/comment.controller.js
@@ -1,6 +1,7 @@
 import SongComment from "../Modules/comment.model.js";
+
 export const saveComment = async (req, res, next) => {
-    const { display_name, userID, time, songID, comment, uri } = req.body;
+    const {display_name, userID, time, songID, comment, uri} = req.body;
     try {
         const newComment = new SongComment({
             display_name,
@@ -17,5 +18,23 @@ export const saveComment = async (req, res, next) => {
         });
     } catch (error) {
         next(error);
+    }
+}
+
+export const getCommentsBySongID = async (req, res, next) => {
+    const {songID} = req.params; // 从URL参数中获取songID
+    try {
+        const comments = await SongComment.find({songID}); // 查找所有与songID匹配的评论
+        if (comments.length === 0) {
+            return res.status(404).json({
+                message: "No comments found for this song."
+            });
+        }
+        res.status(200).json({
+            message: "Comments retrieved successfully.",
+            comments
+        });
+    } catch (error) {
+        next(error); // 传递错误到下一个中间件
     }
 }
--- server/Routes/comment.route.js
+++ server/Routes/comment.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {saveComment} from "../Controllers/comment.controller.js";
+import {getCommentsBySongID, saveComment} from "../Controllers/comment.controller.js";
 
 const commentRoute = express.Router();
-commentRoute.put('/',saveComment);
+commentRoute.put('/', saveComment);
+commentRoute.get('/', getCommentsBySongID);
 
 export default commentRoute;
