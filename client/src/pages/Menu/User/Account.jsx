29af0f39a3df076268ffc1ccbc34fd05b74aba16
GFORGALEN <100574277+GFORGALEN@users.noreply.github.com>
2024-05-07 13:05:48 +1200

    fix button background color

--- client/src/pages/Menu/User/Account.jsx
+++ client/src/pages/Menu/User/Account.jsx
@@ -53,7 +53,7 @@ function Account() {
                         }}
                     />
                         <button
-                            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline"
+                            className="w-full bg-gradient-to-r from-[#594ea0] via-[#6e073b] to-[#ff0000] hover:from-[#4d4087] hover:via-[#5e0631] hover:to-[#cc0000] text-white font-bold py-7 px-6 rounded focus:outline-none focus:shadow-outline"
                             onClick={handlerLogin}
                         >
                             Sign In
