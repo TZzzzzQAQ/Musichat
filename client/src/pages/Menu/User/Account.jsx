05c392c5fa8fed4264d702d748a0e79f33cb5a71
GFORGALEN <100574277+GFORGALEN@users.noreply.github.com>
2024-05-07 16:15:04 +1200

    fix login taliwind version

--- client/src/pages/Menu/User/Account.jsx
+++ client/src/pages/Menu/User/Account.jsx
@@ -1,66 +1,80 @@
-import {useLocation} from "react-router-dom";
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {setUserProfile} from "@/store/features/userSlice.jsx";
-import {useSpotifyAuth} from "@/hooks/useSpotifyAuth.jsx";
-import {sendUserDataAPI} from "@/apis/chatGroupAPI.jsx";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserProfile } from "@/store/features/userSlice.jsx";
+import { useSpotifyAuth } from "@/hooks/useSpotifyAuth.jsx";
+import { sendUserDataAPI } from "@/apis/chatGroupAPI.jsx";
 import Setting from "@/pages/Menu/User/User.jsx";
+const backgroundImageUrl = "https://i.ibb.co/cTxbCWy/spotify-k8mh.png";;
 
-const backgroundImageUrl = "https://i.ibb.co/cTxbCWy/spotify-k8mh.png";
+const BackgroundImage = () => (
+    <div
+      className="absolute inset-0 bg-contain bg-center bg-no-repeat filter blur-[7px] z-[-1] overflow-hidden"
+      style={{ backgroundImage: `url('${backgroundImageUrl}')` }}
+    />
+  );
+
+const LoginHeader = ({ onLogin }) => {
+  const [loginText, setLoginText] = useState("Login to Your Account");
+
+  const handleClick = () => {
+    setLoginText("Login in progress... ");
+    setTimeout(() => {
+      onLogin();
+    }, 1000);
+  };
+
+  return (
+    <h2
+      className="text-[40px] font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 cursor-pointer transition duration-300 ease-in-out hover:opacity-80"
+      onClick={handleClick}
+    >
+      {loginText}
+    </h2>
+  );
+};
 
 function Account() {
-    const location = useLocation();
-    const userState = useSelector((state) => state.user.profile);
-    const dispatch = useDispatch();
-    const {redirectToAuthCodeFlow, fetchProfile} = useSpotifyAuth();
+  const location = useLocation();
+  const userState = useSelector((state) => state.user.profile);
+  const dispatch = useDispatch();
+  const { redirectToAuthCodeFlow, fetchProfile } = useSpotifyAuth();
 
-    useEffect(() => {
-        const query = new URLSearchParams(location.search);
-        const code = query.get("code");
-        if (code && Object.keys(userState).length === 0) {
-            handlerFetchProfile(code);
-        }
-    }, [location, userState]);
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const code = query.get("code");
+    if (code && Object.keys(userState).length === 0) {
+      handlerFetchProfile(code);
+    }
+  }, [location, userState]);
 
-    const handlerLogin = () => {
-        redirectToAuthCodeFlow();
-    };
+  const handlerLogin = () => {
+    redirectToAuthCodeFlow();
+  };
 
-    const handlerFetchProfile = async (code) => {
-        try {
-            const profile = await fetchProfile(code);
-            if (profile?.error?.status === 401) {
-                return;
-            }
-            dispatch(setUserProfile(profile));
-            sendUserDataAPI(profile);
-        } catch (error) {
-            console.error("Failed to fetch profile:", error);
-        }
-    };
+  const handlerFetchProfile = async (code) => {
+    try {
+      const profile = await fetchProfile(code);
+      if (profile?.error?.status === 401) {
+        return;
+      }
+      dispatch(setUserProfile(profile));
+      sendUserDataAPI(profile);
+    } catch (error) {
+      console.error("Failed to fetch profile:", error);
+    }
+  };
 
-    return (
-        <div className="h-full w-full font-poppins">
-            {Object.keys(userState).length === 0 ? (
-                <div className="flex flex-col items-center justify-center h-full w-full ">
-                    <div
-                        className="w-full h-full"
-                        style={{
-                            backgroundImage: `url('${backgroundImageUrl}')`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                            backgroundRepeat: 'no-repeat'
-                        }}
-                    />
-                        <button
-                            className="w-full bg-gradient-to-r from-[#594ea0] via-[#6e073b] to-[#ff0000] hover:from-[#4d4087] hover:via-[#5e0631] hover:to-[#cc0000] text-white font-bold py-7 px-6 rounded focus:outline-none focus:shadow-outline"
-                            onClick={handlerLogin}
-                        >
-                            Sign In
-                        </button>
-                </div>
-            ) : (
-                <div className="flex flex-col items-center p-12">
+  return (
+    <div className="h-full">
+      {Object.keys(userState).length === 0 ? (
+        <div className="relative flex flex-col items-center justify-center h-full overflow-hidden">
+          <BackgroundImage />
+          <LoginHeader onLogin={handlerLogin} />
+          
+        </div>
+      ) : (
+        <div className="flex flex-col items-center p-12">
                     <h2 className="text-6xl mb-5 bg-clip-text text-transparent bg-gradient-to-r from-purple-800 to-pink-500">Welcome, {userState.display_name}!</h2>
                     <Setting/>
                     <div className="flex items-center mt-12">
@@ -79,9 +93,8 @@ function Account() {
                     </div>
                 </div>
             )}
-        </div>
-
-    );
+    </div>
+  );
 }
 
 export default Account;
