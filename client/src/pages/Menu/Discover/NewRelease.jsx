c6875c0bdf56ac75ffb2b61991cdff08e7bfda74
uoa-mliu866 <147148677+uoa-mliu866@users.noreply.github.com>
2024-05-09 12:46:59 +1200

    Update NewRelease.jsx

--- client/src/pages/Menu/Discover/NewRelease.jsx
+++ client/src/pages/Menu/Discover/NewRelease.jsx
@@ -1,28 +1,42 @@
+// Import the useEffect and useState hooks from the React library
 import {useEffect, useState} from 'react';
+// Import the ImageCard component
 import ImageCard from '@/components/ImageCard.jsx';
+// Import the NavLink component for routing purposes
 import {NavLink} from 'react-router-dom';
+// Import the getNewReleasesAPI function to fetch data from the API
 import {getNewReleasesAPI} from "@/apis/everyoneDataAPI.jsx";
+// Import the Loading component to display loading state
 import Loading from "@/components/Loading/Loading.jsx";
 
+// Define the AlbumPage component
 const AlbumPage = () => {
+    // Use useState to define the albums state, initially an empty array
     const [albums, setAlbums] = useState([]);
+    // Use useState to define the searchParams state, containing parameters needed for the API request
     const [searchParams] = useState({
         type: 'album',
         limit: 10,
         market: 'nz'
     })
+    // Use the useEffect hook to handle the data fetching logic
     useEffect(() => {
+        // Define the asynchronous function fetchAlbums to call the API and fetch album data
         const fetchAlbums = async () => {
             try {
+                // Call the API and set the response data to the albums state
                 const response = await getNewReleasesAPI(searchParams)
                 setAlbums(response.albums.items);
             } catch (error) {
+                // Catch and log any errors
                 console.error('Error fetching albums:', error);
             }
         };
+        // Execute the fetchAlbums function
         fetchAlbums();
-    }, [searchParams]);
+    }, [searchParams]);// useEffect depends on searchParams, it re-executes when searchParams change
 
+    // JSX structure returned by the component
     return (
         <div className='overflow-y-auto h-full'>
             {albums.length > 0 ? (
@@ -46,4 +60,5 @@ const AlbumPage = () => {
     );
 };
 
+// Export the AlbumPage component
 export default AlbumPage;
