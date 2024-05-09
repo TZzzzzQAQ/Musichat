b90a209dda5d33a37f4b6e0bc44355e9aae33f88
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-05-09 16:32:38 +1200

    V5.0.2 BackEnd Chat

--- client/src/pages/Menu/GroupChat.jsx
+++ client/src/pages/Menu/GroupChat.jsx
@@ -1,13 +1,9 @@
-// Import socket.io-client and React-related hooks
 import io from 'socket.io-client';
-import { useEffect, useState } from "react";
-// Import the useSelector hook from react-redux to access the state in the Redux store
-import { useSelector } from "react-redux";
-// Import AuthRoute component for route protection, ensuring user authentication
+import {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
 import AuthRoute from "@/components/AuthRoute.jsx";
-// Import getMessageAPI function to fetch message data
-import { getMessageAPI } from "@/apis/messageAPI.jsx";
-import { useRef } from "react";
+import {getMessageAPI} from "@/apis/messageAPI.jsx";
+import {useRef} from "react";
 import {APP_API_URL} from "@/../config.js";
 
 
@@ -43,8 +39,8 @@ const GroupChat = () => {
     useEffect(() => {
         const fetch = async () => {
             const response = await getMessageAPI();
-            setChat(prevState => {
-                return [...prevState, ...response];
+            setChat(() => {
+                return [...response];
             })
         }
         fetch();
@@ -74,7 +70,7 @@ const GroupChat = () => {
     // Use useRef to create a reference for automatically scrolling to the bottom of the chat history
     const messagesEndRef = useRef(null); // Reference to the last message in the chat history
     useEffect(() => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" }); // Automatically scroll to the bottom when chat history updates
+        messagesEndRef.current?.scrollIntoView({behavior: "smooth"}); // Automatically scroll to the bottom when chat history updates
     }, [chat]);
 
     // JSX structure of the component, including the message display area and input area
@@ -83,20 +79,24 @@ const GroupChat = () => {
             <div className="h-full flex flex-col ">
                 <div className="flex-grow overflow-auto p-3 shadow-inner">
                     {chat.map((msg, index) => (
-                        <div key={index} className={`flex mb-4 text-sm items-start ${msg.id === userState.id ? 'justify-end' : 'justify-start'}`}>
-                            <div className={`flex ${msg.id === userState.id ? 'flex-row-reverse' : 'flex-row'} items-center`}>
-                                
-                                <div className={` flex-shrink-0 bg-gray-300 rounded-full w-8 h-8 ${msg.id === userState.id ? 'ml-3' : 'mr-3'}`} >
+                        <div key={index}
+                             className={`flex mb-4 text-sm items-start ${msg.id === userState.id ? 'justify-end' : 'justify-start'}`}>
+                            <div
+                                className={`flex ${msg.id === userState.id ? 'flex-row-reverse' : 'flex-row'} items-center`}>
+
+                                <div
+                                    className={` flex-shrink-0 bg-gray-300 rounded-full w-8 h-8 ${msg.id === userState.id ? 'ml-3' : 'mr-3'}`}>
                                     
                                     <span className="block w-full h-full rounded-full overflow-hidden">
                                         
-                                        <img src={msg.img} alt="avatar" className="w-full h-full object-cover" />
+                                        <img src={msg.img} alt="avatar" className="w-full h-full object-cover"/>
                                     </span>
                                 </div>
                                 <div className="flex-1 overflow-hidden">
                                     <div>
                                         <span className="font-bold text-gray-600 ">{msg.display_name}</span>
-                                        <span className="text-gray-500 text-xs ml-2">{new Date(msg.time).toLocaleTimeString()}</span>
+                                        <span
+                                            className="text-gray-500 text-xs ml-2">{new Date(msg.time).toLocaleTimeString()}</span>
                                     </div>
                                     <p className="text-black leading-normal">{msg.message}</p>
                                 </div>
@@ -104,7 +104,7 @@ const GroupChat = () => {
                         </div>
                     ))}
 
-                    <div ref={messagesEndRef} />
+                    <div ref={messagesEndRef}/>
                 </div>
                 <div className="p-4 max-w-lg mx-auto bg-white shadow-lg rounded-lg">
                     <div className="flex items-center">
